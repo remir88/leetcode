b64e0e066157f677ba356db78bb0c99cc9685700
remir88 <remir8801@gmail.com>
2018-03-16 16:50:16 +0800

    Changes to be committed: 	modified:   numJewelsInStones.py

--- numJewelsInStones.py
+++ numJewelsInStones.py
@@ -1,3 +1,12 @@
 class Solution:
 	def numJewelsInStones(self, J, S):
-		print("hello, world!")
+		totalJewels = 0
+		jewels = {}
+		for letter in J:
+			jewels[letter] = 1
+		for letter in S:
+			if (letter in jewels):
+				totalJewels = totalJewels + 1
+		return(totalJewels)
+
+				
